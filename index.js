33bad79fd9edfb560d69eb61a21188c581b26de1
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-05-14 14:41:03 +0100

    added functionality to display nav in mobile view

---
+++ index.js
@@ -0,0 +1,10 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navBar = document.getElementById("navbar")
+
+
+
+function showNav() {
+    navBar.style.display = "flex";
+
+    console.log("hi");
+}
