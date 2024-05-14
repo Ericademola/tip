64c55dc6e4b30b89a9d203555ee9a3c963d0e317
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-05-14 15:01:11 +0100

    added contional display for nav

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const navBar = document.getElementById("navbar")
 
-
+let displayNav = false;
 
 function showNav() {
-    navBar.style.display = "flex";
-
-    console.log("hi");
+    if (displayNav === false) {
+        navBar.style.display = "flex";
+        displayNav = true;
+    } else {
+        navBar.style.display = "none";
+        displayNav = false;
+    }
 }
